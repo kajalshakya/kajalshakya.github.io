28818467b2407301d227d12324c297c637f4c184
Rohit Shakya <rohit.rkshakya@gmail.com>
2021-01-30 23:46:08 +0530

    added js and css

---
+++ web/resource/sky.js
@@ -0,0 +1,10 @@
+
+  
+function helloworld() {
+  document.getElementById("demo").innerHTML = "Paragraph changed!";
+  for (var i = 0; i <=10; i++) {
+    document.write("2 * "+i+" = "+(2*i)+"<br>");
+  }
+}
+helloworld();
+
