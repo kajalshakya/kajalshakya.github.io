c781c4f465f39286a1aed1e6bc75c9ca4c6fa188
Rohit Shakya <rohit.rkshakya@gmail.com>
2021-02-04 00:18:49 +0530

    added phone.html

--- web/resource/sky.js
+++ web/resource/sky.js
@@ -1,10 +1,7 @@
-
-  
 function helloworld() {
-  document.getElementById("demo").innerHTML = "Paragraph changed!";
-  for (var i = 0; i <=10; i++) {
-    document.write("2 * "+i+" = "+(2*i)+"<br>");
-  }
+	document.getElementById("demo").innerHTML = "Paragraph changed!";
+	for (var i = 0; i <= 10; i++) {
+		document.write("2 * " + i + " = " + (2 * i) + "<br>");
+	}
 }
 helloworld();
-
